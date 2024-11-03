ad110296d3d1942d0680acd6f6babb49408fc915
Patrick Joyce <pjaudiomv@gmail.com>
2024-11-02 22:04:23 -0400

    Update Router.d.ts type for svelte5
    Update Router.d.ts type for svelte5

--- wrap.d.ts
+++ wrap.d.ts
@@ -1,16 +1,16 @@
-import {ComponentType} from 'svelte'
+import {Component} from 'svelte'
 import {AsyncSvelteComponent, RoutePrecondition, WrappedComponent} from './Router'
 
 /** Options object for the call to `wrap` */
 export interface WrapOptions {
     /** Svelte component to load (this is incompatible with `asyncComponent`) */
-    component?: ComponentType
+    component?: Component
 
     /** Function that returns a Promise that fulfills with a Svelte component (e.g. `{asyncComponent: () => import('Foo.svelte')}`)  */
     asyncComponent?: AsyncSvelteComponent
 
     /** Svelte component to be displayed while the async route is loading (as a placeholder); when unset or false-y, no component is shown while component */
-    loadingComponent?: ComponentType
+    loadingComponent?: Component
 
     /** Optional dictionary passed to the `loadingComponent` component as params (for an exported prop called `params`) */
     loadingParams?: object
